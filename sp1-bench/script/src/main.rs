0cdd5df954fc1bf23704c8865d0a79a92034e178
sidtuladhar <smt9598@nyu.edu>
2024-11-02 16:58:09 +0800

    fix sp1

--- sp1-bench/script/src/main.rs
+++ sp1-bench/script/src/main.rs
@@ -1,26 +1,91 @@
-use sp1_sdk::{utils, ProverClient, SP1Stdin};
+//! An end-to-end example of using the SP1 SDK to generate a proof of a program that can be executed
+//! or have a core proof generated.
+//!
+//! You can run this script using the following command:
+//! ```shell
+//! RUST_LOG=info cargo run --release -- --execute
+//! ```
+//! or
+//! ```shell
+//! RUST_LOG=info cargo run --release -- --prove
+//! ```
 
-/// The ELF we want to execute inside the zkVM.
-const ELF: &[u8] = include_bytes!("../../program/elf/riscv32im-succinct-zkvm-elf");
+use alloy_sol_types::SolType;
+use clap::Parser;
+use fibonacci_lib::PublicValuesStruct;
+use sp1_sdk::{ProverClient, SP1Stdin};
+
+/// The ELF (executable and linkable format) file for the Succinct RISC-V zkVM.
+pub const FIBONACCI_ELF: &[u8] = include_bytes!("../../elf/riscv32im-succinct-zkvm-elf");
+
+/// The arguments for the command.
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Args {
+    #[clap(long)]
+    execute: bool,
+
+    #[clap(long)]
+    prove: bool,
+
+    #[clap(long, default_value = "20")]
+    n: u32,
+}
 
 fn main() {
-    // Setup a tracer for logging.
-    // utils::setup_logger();
+    // Setup the logger.
+    sp1_sdk::utils::setup_logger();
 
-    // Create an input stream.
-    let stdin = SP1Stdin::new();
+    // Parse the command line arguments.
+    let args = Args::parse();
 
-    // Generate the proof for the given program.
+    if args.execute == args.prove {
+        eprintln!("Error: You must specify either --execute or --prove");
+        std::process::exit(1);
+    }
+
+    // Setup the prover client.
     let client = ProverClient::new();
-    // client.prove(ELF, stdin).expect("proving failed");
 
-    // Verify proof.
-    client.verify(ELF, &proof).expect("verification failed");
+    // Setup the inputs.
+    let mut stdin = SP1Stdin::new();
+    stdin.write(&args.n);
 
-    // Save the proof.
-    // proof
-    //     .save("proof-with-pis.json")
-    //     .expect("saving proof failed");
+    println!("n: {}", args.n);
 
-    println!("successfully generated and verified proof for the program!")
+    if args.execute {
+        // Execute the program
+        let (output, report) = client.execute(FIBONACCI_ELF, stdin).run().unwrap();
+        println!("Program executed successfully.");
+
+        // Read the output.
+        let decoded = PublicValuesStruct::abi_decode(output.as_slice(), true).unwrap();
+        let PublicValuesStruct { n, a, b } = decoded;
+        println!("n: {}", n);
+        println!("a: {}", a);
+        println!("b: {}", b);
+
+        let (expected_a, expected_b) = fibonacci_lib::fibonacci(n);
+        assert_eq!(a, expected_a);
+        assert_eq!(b, expected_b);
+        println!("Values are correct!");
+
+        // Record the number of cycles executed.
+        println!("Number of cycles: {}", report.total_instruction_count());
+    } else {
+        // Setup the program for proving.
+        let (pk, vk) = client.setup(FIBONACCI_ELF);
+
+        // Generate the proof
+        let proof = client
+            .prove(&pk, stdin)
+            .run()
+            .expect("failed to generate proof");
+
+        println!("Successfully generated proof!");
+
+        // Verify the proof.
+        client.verify(&proof, &vk).expect("failed to verify proof");
+        println!("Successfully verified proof!");
+    }
 }
