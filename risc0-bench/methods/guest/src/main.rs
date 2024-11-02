aa1a8304191393092811dc520641ecd1c564a81c
sidtuladhar <smt9598@nyu.edu>
2024-11-02 15:55:39 +0800

    optimize risc0

--- risc0-bench/methods/guest/src/main.rs
+++ risc0-bench/methods/guest/src/main.rs
@@ -1,18 +1,16 @@
-extern crate alloc;
-use alloc::vec::Vec;
 use risc0_zkvm::guest::env;
 
-fn fib(n: u32) -> u32 {
-    let mut nums = Vec::<u32>::new(); // Initialize an empty vector
-    nums.push(1); // Manually push the first two Fibonacci numbers
-    nums.push(1);
-
-    // Start the loop from the third Fibonacci number
-    for _ in 2..n {
-        let c = (nums[nums.len() - 1] + nums[nums.len() - 2]) % 7919;
-        nums.push(c);
+fn fib(n: u32) -> u128 {
+    let mut a: u128 = 0;
+    let mut b: u128 = 1;
+    let mut sum: u128;
+    for _ in 1..n {
+        sum = a + b;
+        a = b;
+        b = sum;
     }
-    nums[nums.len() - 1]
+
+    b
 }
 
 fn main() {
