2cac001e33fbc62e88b6aed5dcbf8a05e6367c38
sidtuladhar <smt9598@nyu.edu>
2024-11-02 17:05:15 +0800

    test

--- jolt-bench/src/main.rs
+++ jolt-bench/src/main.rs
@@ -1,8 +1,9 @@
 pub fn main() {
-    let (prove_fib, _verify_fib) = guest::build_fib();
+    let (prove_fib, verify_fib) = guest::build_fib();
 
-    let (output, _proof) = prove_fib(10);
-    // let is_valid = verify_fib(proof);
+    let (output, proof) = prove_fib(10);
+    let is_valid = verify_fib(proof);
+
     println!("output: {}", output);
-    // println!("valid: {}", is_valid);
+    println!("valid: {}", is_valid);
 }
--- sp1-bench/program/src/main.rs
+++ sp1-bench/program/src/main.rs
@@ -1,23 +1,30 @@
+//! A simple program that takes a number `n` as input, and writes the `n-1`th and `n`th fibonacci
+//! number as an output.
+
+// These two lines are necessary for the program to properly compile.
+//
+// Under the hood, we wrap your main function with some extra code so that it behaves properly
+// inside the zkVM.
 #![no_main]
-// #![no_std]
 sp1_zkvm::entrypoint!(main);
-extern crate alloc;
-use alloc::vec::Vec;
 
-fn fib(n: u32) -> u32 {
-    let mut nums = Vec::<u32>::new(); // Initialize an empty vector
-    nums.push(1); // Manually push the first two Fibonacci numbers
-    nums.push(1);
-
-    // Start the loop from the third Fibonacci number
-    for _ in 2..n {
-        let c = (nums[nums.len() - 1] + nums[nums.len() - 2]) % 7919;
-        nums.push(c);
-    }
-    nums[nums.len() - 1]
-}
+use alloy_sol_types::SolType;
+use fibonacci_lib::{fibonacci, PublicValuesStruct};
 
 pub fn main() {
-    let output = fib(1000);
-    println!("output: {}", output);
+    // Read an input to the program.
+    //
+    // Behind the scenes, this compiles down to a custom system call which handles reading inputs
+    // from the prover.
+    let n = sp1_zkvm::io::read::<u32>();
+
+    // Compute the n'th fibonacci number using a function from the workspace lib crate.
+    let (a, b) = fibonacci(n);
+
+    // Encode the public values of the program.
+    let bytes = PublicValuesStruct::abi_encode(&PublicValuesStruct { n, a, b });
+
+    // Commit to the public values of the program. The final proof will have a commitment to all the
+    // bytes that were committed to.
+    sp1_zkvm::io::commit_slice(&bytes);
 }
