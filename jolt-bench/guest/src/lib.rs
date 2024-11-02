95c1129697970056c11f2602c0e8c70bb826add6
sidtuladhar <smt9598@nyu.edu>
2024-11-02 17:05:15 +0800

    test

--- jolt-bench/guest/src/lib.rs
+++ jolt-bench/guest/src/lib.rs
@@ -1,19 +1,15 @@
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
 
-extern crate alloc;
-use alloc::vec::Vec;
-
 #[jolt::provable]
-fn fib(n: u32) -> u32 {
-    let mut nums = Vec::<u32>::new(); // Initialize an empty vector
-    nums.push(1); // Manually push the first two Fibonacci numbers
-    nums.push(1);
-
-    // Start the loop from the third Fibonacci number
-    for _ in 2..n {
-        let c = (nums[nums.len() - 1] + nums[nums.len() - 2]) % 7919;
-        nums.push(c);
+fn fib(n: u32) -> u128 {
+    let mut a: u128 = 0;
+    let mut b: u128 = 1;
+    let mut sum: u128;
+    for _ in 1..n {
+        sum = a + b;
+        a = b;
+        b = sum;
     }
-    nums[nums.len() - 1]
+    b
 }
