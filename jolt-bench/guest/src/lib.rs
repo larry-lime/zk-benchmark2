e8da17d1d6897bd871426b0635bec115aa7849b9
sidtuladhar <smt9598@nyu.edu>
2024-11-26 17:40:29 +0800

    add python:

--- jolt-bench/guest/src/lib.rs
+++ jolt-bench/guest/src/lib.rs
@@ -1,5 +1,34 @@
+use pyo3::prelude::*;
+use pyo3::types::PyModule;
+use std::path::Path;
+
 #![cfg_attr(feature = "guest", no_std)]
 #![no_main]
+
+
+fn executePython<P: AsRef<Path>>(python_script_path: P) -> PyResult<String> {
+    Python::with_gil(|py| {
+        // Read the Python script content
+        let script_content = std::fs::read_to_string(&python_script_path)
+            .map_err(|e| pyo3::exceptions::PyIOError::new_err(format!(
+                "Failed to read Python script: {}",
+                e
+            )))?;
+        
+        // Import the Python script as a module
+        let script = PyModule::from_code(
+            py,
+            &script_content,
+            python_script_path.as_ref().to_str().unwrap_or("python.py"),
+            "python",
+        )?;
+        
+        // Call the `train_and_save_model` function without arguments
+        let result: String = script.call1("python", ())?.extract()?;
+        
+        Ok(result)
+    })
+}
 
 #[jolt::provable]
 fn fib(n: u32) -> u128 {
@@ -13,3 +42,12 @@ fn fib(n: u32) -> u128 {
     }
     b
 }
+
+#[jolt::provable]
+fn add(a: u128, b: u128) -> u128 {
+    let mut sum: u128;
+
+    sum = a + b;
+
+    sum
+  }
--- jolt-bench/src/main.rs
+++ jolt-bench/src/main.rs
@@ -1,9 +1,28 @@
-pub fn main() {
-    let (prove_fib, verify_fib) = guest::build_fib();
-
-    let (output, proof) = prove_fib(10);
-    let is_valid = verify_fib(proof);
-
-    println!("output: {}", output);
-    println!("valid: {}", is_valid);
+pub fn main() -> PyResult<()> {
+    //  // let (prove_fib, verify_fib) = guest::build_fib();
+    // let (prove_add, verify_add) = guest::build_add();
+    //
+    //  //let (output, proof) = prove_fib(10);
+    // let (add_output, add_proof) = prove_add(1, 2);
+    // let (add2_output, add2_proof) = prove_add(2, 3);
+    //
+    //  //let is_valid = verify_fib(proof);
+    // let add_is_valid = verify_add(add_proof);
+    // let try_add = verify_add(add2_proof);
+    //
+    // //println!("output: {}", output);
+    // //println!("valid: {}", is_valid);
+    //
+    // println!("add output: {}", add_output);
+    // println!("add valid: {}", add_is_valid);
+    let python_script = Path::new("./model/python.py");
+    
+    // Execute the Python function and capture the result
+    let result = executePython(python_script)?;
+    
+    // Print the result
+    println!("{}", result); // Expected Output: Models trained and saved successfully.
+    
+    Ok(())
+    
 }
