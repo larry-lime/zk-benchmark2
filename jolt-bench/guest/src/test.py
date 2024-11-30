3cb3d1204b0db938a7932475322b87e54ec4711a
sidtuladhar <smt9598@nyu.edu>
2024-11-30 21:02:37 +0800

    working PyO3

--- jolt-bench/guest/src/test.py
+++ jolt-bench/guest/src/test.py
@@ -2,8 +2,11 @@ import joblib
 import os
 
 def import_model():
-    print(os.getcwd());
     loaded_linear_reg = joblib.load('./model/ridge_reg_model_sklearn.joblib')
     loaded_ridge_reg = joblib.load('./model/ridge_reg_model_sklearn.joblib')
     loaded_scaler = joblib.load('./model/standard_scaler_sklearn.joblib')
-    print("Loaded")
+    return "LOADED MODEL"
+
+
+if __name__ == "__main__":
+    print(import_model())
