a77224c831d4a695ebdd3ee78f7736df15187419
sidtuladhar <smt9598@nyu.edu>
2024-12-04 15:47:10 +0800

    finish risc0 working

---
+++ risc0/methods/guest/src/main.rs
@@ -0,0 +1,29 @@
+#![no_main]
+#![no_std]
+risc0_zkvm::guest::entry!(main);
+use risc0_zkvm::guest::env;
+
+extern crate alloc;
+use alloc::vec;
+use alloc::vec::Vec;
+
+use serde::{Serialize,  Deserialize};
+use ml_core::{ModelInput};
+
+
+fn main() {
+
+    // read the input
+    let model_input: ModelInput = env::read();
+
+    let X_scaled = model_input.scaler.transform(&model_input.x);
+    
+    let ridge_pred = model_input.ridge_model.predict(&X_scaled);
+    
+    let combined_predictions = vec![ridge_pred.clone()];
+
+    let output: Vec<_> = combined_predictions.into_iter().flat_map(|array| array.to_vec()).collect();
+    // write public output to the journal
+    env::commit(&output);
+    
+}
