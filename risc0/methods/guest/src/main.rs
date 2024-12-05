5e100f426c50bec0c6b8d040c3b60235ec6be223
Lawrence Lim <ll4715@nyu.edu>
2024-12-05 19:14:31 +0800

    printing risc0 output

--- risc0/methods/guest/src/main.rs
+++ risc0/methods/guest/src/main.rs
@@ -7,23 +7,23 @@ extern crate alloc;
 use alloc::vec;
 use alloc::vec::Vec;
 
-use serde::{Serialize,  Deserialize};
-use ml_core::{ModelInput};
-
+use ml_core::ModelInput;
+use serde::{Deserialize, Serialize};
 
 fn main() {
-
     // read the input
     let model_input: ModelInput = env::read();
 
     let X_scaled = model_input.scaler.transform(&model_input.x);
-    
+
     let ridge_pred = model_input.ridge_model.predict(&X_scaled);
-    
+
     let combined_predictions = vec![ridge_pred.clone()];
 
-    let output: Vec<_> = combined_predictions.into_iter().flat_map(|array| array.to_vec()).collect();
+    let output: Vec<_> = combined_predictions
+        .into_iter()
+        .flat_map(|array| array.to_vec())
+        .collect();
     // write public output to the journal
     env::commit(&output);
-    
 }
